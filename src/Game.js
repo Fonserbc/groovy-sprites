d1d3021880a54a8896f344994728db01f712b006
Fonserbc <fonserbc@gmail.com>
2014-08-08 15:24:19 +0200

    Added interaction and 4th sprite

--- src/Game.js
+++ src/Game.js
@@ -7,6 +7,7 @@ require([
 	'goo/renderer/Camera',
 	'goo/entities/components/CameraComponent',
 	'goo/entities/components/ScriptComponent',
+	'goo/math/Vector3',
 	
 	'src/GroovySprite'
 
@@ -18,6 +19,7 @@ require([
 	Camera,
 	CameraComponent,
 	ScriptComponent,
+	Vector3,
 	
 	GroovySprite
 ) {
@@ -70,6 +72,26 @@ require([
 		minFilter: 'NearestNeighborNoMipMaps',
 		generateMipmaps: 'false'
 	};
+	var textureSettings3 = {
+		magFilter: 'NearestNeighbor',
+		minFilter: 'NearestNeighborNoMipMaps',
+		generateMipmaps: 'false'
+	};
+	var textureSettingsGroovy3 = {
+		magFilter: 'NearestNeighbor',
+		minFilter: 'NearestNeighborNoMipMaps',
+		generateMipmaps: 'false'
+	};
+	var textureSettings4 = {
+		magFilter: 'NearestNeighbor',
+		minFilter: 'NearestNeighborNoMipMaps',
+		generateMipmaps: 'false'
+	};
+	var textureSettingsGroovy4 = {
+		magFilter: 'NearestNeighbor',
+		minFilter: 'NearestNeighborNoMipMaps',
+		generateMipmaps: 'false'
+	};
 	
 	var castleTexturePromise = new RSVP.Promise();
 	var castleGroovyPromise = new RSVP.Promise();
@@ -77,80 +99,167 @@ require([
 	var towerGroovyPromise = new RSVP.Promise();
 	var screwTexturePromise = new RSVP.Promise();
 	var screwGroovyPromise = new RSVP.Promise();
+	var gooTexturePromise = new RSVP.Promise();
+	var gooGroovyPromise = new RSVP.Promise();
 	
 	var textureCreator = new TextureCreator();
 	var castleTexture = textureCreator.loadTexture2D('res/sprites/castle.png',textureSettings, function () { castleTexturePromise.resolve()});
 	var castleGroovyTexture = textureCreator.loadTexture2D('res/sprites/castle_h.png',textureSettingsGroovy, function () { castleGroovyPromise.resolve();});
 	var towerTexture = textureCreator.loadTexture2D('res/sprites/tower.png',textureSettings2, function () { towerTexturePromise.resolve()});
 	var towerGroovyTexture = textureCreator.loadTexture2D('res/sprites/tower_h.png',textureSettingsGroovy2, function () { towerGroovyPromise.resolve();});
-	var screwTexture = textureCreator.loadTexture2D('res/sprites/screw.png',null, function () {	screwTexturePromise.resolve()});
-	var screwGroovyTexture = textureCreator.loadTexture2D('res/sprites/screw_h.png',null, function () {	screwGroovyPromise.resolve()}); 
+	var screwTexture = textureCreator.loadTexture2D('res/sprites/screw.png',textureSettings3, function () {	screwTexturePromise.resolve()});
+	var screwGroovyTexture = textureCreator.loadTexture2D('res/sprites/screw_h.png',textureSettingsGroovy3, function () {	screwGroovyPromise.resolve()});
+	var gooTexture = textureCreator.loadTexture2D('res/sprites/goo_logo.png',textureSettings4, function () { gooTexturePromise.resolve()});
+	var gooGroovyTexture = textureCreator.loadTexture2D('res/sprites/goo_logo_h.png',textureSettingsGroovy4, function () { gooGroovyPromise.resolve()}); 
 	
-	new RSVP.all([castleTexturePromise, castleGroovyPromise, towerTexturePromise, towerGroovyPromise, screwTexturePromise, screwGroovyPromise]).then(init);
+	new RSVP.all([
+		castleTexturePromise, castleGroovyPromise,
+		towerTexturePromise, towerGroovyPromise,
+		screwTexturePromise, screwGroovyPromise,
+		gooTexturePromise, gooGroovyPromise
+	]).then(init);
 
-	var grooveDir = [0,0,0];
-
-	var lastMouse = {x: 0, y: 0};
-	var changeGroove = false;
-
-	document.addEventListener('mousemove', function (e) {
-		if (!changeGroove) return;
-		grooveDir [0] -= (e.pageX - lastMouse.x)/300;
-		grooveDir [2] += (e.pageY - lastMouse.y)/300;
-
-		lastMouse.x = e.pageX;
-		lastMouse.y = e.pageY;
-
-		grooveDir[0] = Math.max(-1.0, Math.min(1.0, grooveDir[0]));
-		grooveDir[2] = Math.max(-1.0, Math.min(1.0, grooveDir[2]));
-	});
-
-	document.addEventListener('mousedown', function (e) {
-		lastMouse.x = e.pageX;
-		lastMouse.y = e.pageY;
-
-		changeGroove = !changeGroove;
-	});
-
-	document.addEventListener('mousewheel', function (e) {
-		if (!changeGroove) return;
-		lastMouse.x = e.pageX;
-		lastMouse.y = e.pageY;
-
-		grooveDir[1] += Math.max(-1.0, Math.min(1.0, -e.wheelDelta || e.detail)) * 0.035;
-		grooveDir[1] = Math.max(-1.0, Math.min(0.0, grooveDir[1]));
-	});
 	
+	var centerRotation = 0;
+	var rotationSpeed = 1;
+	var rotationDrag = 1;
+	var impulse = 1;
+
+	var mouseStatus = {
+		down: false,
+		downTime: 0.0,
+		moveTime: 0.0,
+		moved: false,
+		lastMouseDown: new Vector3()
+	};
+	window.ms = mouseStatus;
+
+	function mousedown (event) {
+		mouseStatus.down = true;
+		mouseStatus.downTime = goo.world.time;
+		mouseStatus.moveTime = goo.world.time;
+		mouseStatus.lastMouseDown.x = event.pageX || event.touches[0].clientX;
+		mouseStatus.lastMouseDown.y = event.pageY || event.touches[0].clientY;
+	}
+
+	function mousemove (event) {
+		var pos = new Vector3(
+			event.pageX || event.touches[0].clientX,
+			event.pageY || event.touches[0].clientY,
+			0.0
+		);
+		var diffTime = goo.world.time - mouseStatus.moveTime;
+		if (diffTime > 0) {
+
+			// Sometimes when touch screens, this event fires but the user
+			// has not moved the finger (really low movement, now 10 px)
+			if (pos.distance(mouseStatus.lastMouseDown) > 10) {
+				mouseStatus.moved = true;
+
+				if (mouseStatus.down) {
+					var sign = (pos.y > goo.renderer.domElement.height/2)? -1 : 1;
+					var speed = sign*((mouseStatus.lastMouseDown.x - pos.x)/goo.renderer.domElement.width)/diffTime;
+					
+					if (rotationSpeed*speed < 0) { // Changing speed
+						rotationSpeed = speed;
+					}
+					else {
+						var sign = (speed < 0)? -1 : 1;
+						rotationSpeed = sign*Math.max(Math.abs(rotationSpeed), Math.abs(speed));
+					}
+				}
+			}
+		}			
+
+		mouseStatus.moveTime = goo.world.time;
+		mouseStatus.lastMouseDown.x = event.pageX || event.touches[0].clientX;
+		mouseStatus.lastMouseDown.y = event.pageY || event.touches[0].clientY;
+	}
+
+	function mouseup (event) {
+		var diffTime = goo.world.time - mouseStatus.downTime;
+
+		if ((!mouseStatus.moved || diffTime < 0.2) && diffTime < 0.5) { // Tap/fast click
+			rotationSpeed += Math.random()*impulse;
+		}
+
+		mouseStatus.moved = event.touches && event.touches.length == 0;
+		mouseStatus.down = event.touches && event.touches.length != 0;
+	}
+
+	document.addEventListener('mousedown', mousedown);
+	document.addEventListener('touchstart', mousedown);
+	document.addEventListener('mousemove', mousemove);
+	document.addEventListener('touchmove', mousemove);
+	document.addEventListener('mouseup', mouseup);
+	document.addEventListener('touchend', mouseup);
+
 	function run (entity) {
 		var t = entity._world.time;
+
+		var pos = new Vector3().copy(entity.transformComponent.worldTransform.translation);
+		pos.normalize();
+		pos.z = entity.height;
+
+		entity.gameObject.material.shader.uniforms.grooveDir = [pos.x, pos.z, pos.y];
+		entity.setRotation([0,0,-centerRotation])
+	}
+
+	function rotate (entity) {
+		var t = entity._world.time;
 		var tpf = entity._world.tpf;
-		
-		entity.gameObject.material.shader.uniforms.grooveDir = grooveDir; //[Math.sin(t/2.0),1.0,-Math.sin(t/3.0)]
+
+		var frameDrag = tpf*rotationDrag;
+		rotationSpeed = rotationSpeed*(1.0 - frameDrag) + 0.0*frameDrag;
+		centerRotation += tpf*rotationSpeed;
+
+		entity.setRotation([0,0,centerRotation]);
+		entity.setUpdated();
 	}
 
 	function init() {
+		var center = goo.world.createEntity();
+		center.addToWorld();
+		center.set([0,0,0]);
+		center.setComponent(new ScriptComponent({run: rotate}));
+
 		var castleRatio = castleTexture.image.height / castleTexture.image.width;
 		var castleLevels = 8;
 		var castle = new GroovySprite(goo, 1/castleRatio, 1, castleLevels, 1, castleTexture, castleGroovyTexture);
-		castle.entity.set([-1, -0.5, 0]);
+		castle.entity.set([0, 1, 0]);
 		castle.entity.setComponent(new ScriptComponent({run: run}));
+		center.transformComponent.attachChild(castle.entity.transformComponent);
+		castle.entity.height = 0.4;
 
 		var towerRatio = towerTexture.image.height / towerTexture.image.width;
 		var towerLevels = 8;
 		var tower = new GroovySprite(goo, 1/towerRatio, 1, towerLevels, 1, towerTexture, towerGroovyTexture);
-		tower.entity.set([1, -0.5, 0]);
+		tower.entity.set([1, 0, 0]);
 		tower.entity.setComponent(new ScriptComponent({run: run}));
+		center.transformComponent.attachChild(tower.entity.transformComponent);
+		tower.entity.height = 0.5;
 
-		var screwLevels = 16;
+		var screwLevels = 8;
 		var screwRatio = screwTexture.image.height / screwTexture.image.width;
 		var screw = new GroovySprite(goo, 1/screwRatio, 1, screwLevels, (screwTexture.image.width/castleTexture.image.width)/(screwLevels/castleLevels), screwTexture, screwGroovyTexture);
-		screw.entity.set([0, 1, 0]);
+		screw.entity.set([0, -1, 0]);
 		screw.entity.setComponent(new ScriptComponent({run: run}));
+		center.transformComponent.attachChild(screw.entity.transformComponent);
+		screw.entity.height = 0.2;
+
+		var gooLevels = 8;
+		var gooRatio = gooTexture.image.height / gooTexture.image.width;
+		var gooLogo = new GroovySprite(goo, 1/gooRatio, 1, gooLevels, (gooTexture.image.width/castleTexture.image.width)/(gooLevels/castleLevels), gooTexture, gooGroovyTexture);
+		gooLogo.entity.set([-1, 0, 0]);
+		gooLogo.entity.setComponent(new ScriptComponent({run: run}));
+		center.transformComponent.attachChild(gooLogo.entity.transformComponent);
+		gooLogo.entity.height = 0.1;
+
 		
 		var camera = new Camera();
 		camera.setProjectionMode(Camera.Parallel);
 		goo.worldWidth = goo.worldHeight / castleRatio;
-		camera.setFrustum(0, 10, -goo.worldWidth/2, goo.worldWidth/2, goo.worldHeight/2, -goo.worldHeight/2);
+		camera.setFrustum(0, 10, -1.2*goo.worldWidth/2, 1.2*goo.worldWidth/2, 1.2*goo.worldHeight/2, -1.2*goo.worldHeight/2);
 		
 		var cameraEntity = goo.world.createEntity("CameraEntity");
 		cameraEntity.setComponent(new CameraComponent(camera));
